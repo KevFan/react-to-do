46d813750e3bdf1b2bce2e58e729b487f95f8910
Kevin Fan <kevin.fan@errigal.com>
2019-06-16 19:06:16 +0100

    add gh pages to deploy to gh pages + use hash routing

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from "./user/Login";
 import TodoHome from "./todo/TodoHome";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 import SignUp from "./user/SignUp";
 import CustomSnackbar from "./layout/CustomSnackBar";
 import "../css/App.css";
@@ -10,11 +10,11 @@ import {HOME, LOGIN, SIGNUP} from "../constants/RouterRoutes";
 function App() {
   return (
       <div>
-        <Router>
+        <HashRouter basename='/'>
           <Route exact path={LOGIN} component={Login}/>
           <Route exact path={SIGNUP} component={SignUp}/>
           <Route exact path={HOME} component={TodoHome}/>
-        </Router>
+        </HashRouter>
         <CustomSnackbar/>
       </div>
   );
