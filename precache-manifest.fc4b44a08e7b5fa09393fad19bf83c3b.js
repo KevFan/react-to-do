d233b8406908979064c65ed065e546fe10833f5b
Kevin Fan <kevin.fan@errigal.com>
2019-06-16 17:51:05 +0100

    Updates

---
+++ precache-manifest.fc4b44a08e7b5fa09393fad19bf83c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070ae4137b2a57614c7407781fdf4020",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "ec333528d49276480b5c",
+    "url": "/react-to-do/static/css/main.95c5f634.chunk.css"
+  },
+  {
+    "revision": "21799dda0ef9a078081c",
+    "url": "/react-to-do/static/js/2.3a4aa80b.chunk.js"
+  },
+  {
+    "revision": "ec333528d49276480b5c",
+    "url": "/react-to-do/static/js/main.0567cec7.chunk.js"
+  },
+  {
+    "revision": "26b717aa96a48dc90608",
+    "url": "/react-to-do/static/js/runtime~main.8def430e.js"
+  }
+]);
